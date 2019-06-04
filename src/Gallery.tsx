457b97b838cec1eb52533bc1f3dc5ec88c7c3471
Leo Nistor <leo.nistor@gmail.com>
2019-06-04 16:48:42 +0300

    updated index useEffect

--- src/Gallery.tsx
+++ src/Gallery.tsx
@@ -4,6 +4,16 @@ import PICTURES from './data/pictures'
 function Gallery() {
   const [index, setIndex] = useState(0)
 
+  useEffect(() => {
+    setInterval(() => {
+      setIndex(storedIndex => {
+        return (storedIndex + 1) % PICTURES.length
+      })
+    }, 2000)
+  }, [])
+
+  // console.log('index', index)
+
   return (
     <div className="Gallery">
       <img src={PICTURES[index].image} alt="gallery" />
