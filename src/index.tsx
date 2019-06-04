d200fc9ad196053e4e374c821e1039c118435e7b
Leo Nistor <leo.nistor@gmail.com>
2019-06-04 14:08:14 +0300

    arrow vs function

--- src/index.tsx
+++ src/index.tsx
@@ -4,3 +4,20 @@ import './index.css'
 import App from './App'
 
 ReactDOM.render(<App />, document.getElementById('app'))
+
+function createObject(this: Object | void) {
+  console.log('outermost this', this)
+  return {
+    arrowFunction: () => {
+      console.log('arrowFunction this: ', undefined)
+    },
+    functionKeywordFunction: function(this) {
+      console.log('functionKeywordFunction this,', this)
+    }
+  }
+}
+
+const obj = createObject()
+console.log('obj', obj)
+obj.arrowFunction()
+obj.functionKeywordFunction()
