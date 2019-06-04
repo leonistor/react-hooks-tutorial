930fcfbcf1b11689184aca85a80f88f4795a9909
Leo Nistor <leo.nistor@gmail.com>
2019-06-04 14:08:25 +0300

    cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -4,20 +4,3 @@ import './index.css'
 import App from './App'
 
 ReactDOM.render(<App />, document.getElementById('app'))
-
-function createObject(this: Object | void) {
-  console.log('outermost this', this)
-  return {
-    arrowFunction: () => {
-      console.log('arrowFunction this: ', undefined)
-    },
-    functionKeywordFunction: function(this) {
-      console.log('functionKeywordFunction this,', this)
-    }
-  }
-}
-
-const obj = createObject()
-console.log('obj', obj)
-obj.arrowFunction()
-obj.functionKeywordFunction()
